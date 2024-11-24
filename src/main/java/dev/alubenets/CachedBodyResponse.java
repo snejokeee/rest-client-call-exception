c8a7b7b54028797e39fb6702095672e1210cab0c
Aleksey Lubenets <an.lubenets@gmail.com>
2024-11-24 22:36:54 +0300

    add MIT License

--- src/main/java/dev/alubenets/CachedBodyResponse.java
+++ src/main/java/dev/alubenets/CachedBodyResponse.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright © 2024. Aleksey Lubenets <alubenets.dev>
+ * Copyright (c) 2024. Aleksey Lubenets <alubenets.dev>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
